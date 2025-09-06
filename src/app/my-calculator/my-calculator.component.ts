7e3aa2a9c4fd661a7ee9c08946b647a8c66f8489
Yury <yury.s.nazarov@yandex.ru>
2025-09-06 10:15:24 +0300

    calcolator: template ui

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,35 +1,81 @@
 import { Component } from '@angular/core';
 
+interface CalcGroup {
+  first: CalcVar,
+  second: CalcVar,
+  operations: CalcOperations,
+}
+
+interface CalcVar {
+  value:  number,
+  modificator: CalcModifiers,
+}
+
+// enum - строгое перечисление
+enum CalcOperations {
+  plus = '+',
+  minus = '-',
+  multiply = '*',
+  divide = '/ '
+}
+
+enum CalcModifiers {
+  none = 'none',
+  sin = 'sin',
+  cos = 'cos',
+  square = 'square'
+}
+
 @Component({
   selector: 'app-my-calculator',
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.scss'
 })
 export class MyCalculatorComponent {
-  public first: number = 1;
-  public second: number = 1;
-  public operation: string = '+';
-  public operations: string[] = ['+', '-', '*', '/'];
-  //public result: number | undefined = undefined;
+  public calcOperations = CalcOperations;
+  public calcMidifiers = CalcModifiers;
+  // Массив для того, что бы можно было добавить несколько калькуляторов
+  public calcGroups: CalcGroup[] = [
+    // Значения по умолчанию
+    {
+      first: {
+        value: 5,
+        modificator: CalcModifiers.none,
+      },
+      second: {
+        value: 5,
+        modificator: CalcModifiers.none,
+      },
+      operations: CalcOperations.plus
+    },
+  ];
+
+  // История с инициализируемым значением - пустой массив
+  public history: string[] = [];
+
+  public operationsBetweenGroups: CalcOperations[] = [];
+
   public result?: number;
 
-  public calc() {
-    if (this.operation) {
-      switch (this.operation) {
-        case '+':
-          this.result = this.first + this.second;
-          break;
-        case '-':
-          this.result = this.first - this.second;
-          break;
-        case '*':
-          this.result = this.first * this.second;
-          break;
-        case '/':
-          this.result = this.first / this.second;
-          break;
+  // 15-24
+  // Метод для добавлении еще одной группы калькулятора
+  public addGroup(): void {
+    this.calcGroups.push({
+      first: {
+        value: 0,
+        modificator: CalcModifiers.none,
+      },
+      second: {
+        value: 0,
+        modificator: CalcModifiers.none,
+      },
+      operations: CalcOperations.plus
+    })
 
-      }
-    }
+    this.operationsBetweenGroups.push(CalcOperations.plus);
+  }
+  // Метод для удаления группы калькулятора
+  public removeGroup(index: number): void {
+    this.calcGroups.splice(index, 1);
   }
 }
